91e334f2bbc7bd985feb25cee5f930b6cbc7575a
rgbweb <mail@rgbweb.de>
2015-06-12 21:57:01 +0200

    Fixed crash when Console.ReadKey in not available
    Console.ReadKey throws an InvalidOperationException on AppHarbor

--- SCLoader/Program.cs
+++ SCLoader/Program.cs
@@ -26,7 +26,7 @@ namespace SCLoader
 
             ILogger logger = providerManager.GetLogger(settings.LoggerName);
             logger.Initialize();
-            logger.LogVerbose ("Logger [{0}] initialized.", logger.LoggerName);
+            logger.LogVerbose("Logger [{0}] initialized.", logger.LoggerName);
 
             IStorageProvider storageProvider = providerManager.GetStorageProvider(settings.StorageProviderName);
             storageProvider.Initialize();
@@ -43,11 +43,8 @@ namespace SCLoader
                 scLoaderTimer.Start();
             });
 
-            // Wait for ESC-key while the application runs...
-            do
-            {
-                logger.LogInformation("Press [ESC] to close SCLoader.");
-            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            // Run the application until anyone wants to stop it
+            WaitForApplicationCloseCommand();
 
             scLoaderTimer.Stop();
 
@@ -55,6 +52,31 @@ namespace SCLoader
 
             // ExitCode != 0 will cause an automatic restart in some services like AppHarbor Background Workers
             Environment.ExitCode = 1;
+
+        }
+
+        static void WaitForApplicationCloseCommand()
+        {
+
+            try
+            {
+
+                // If we have a console, we wait for the ESC key to be pressed
+                do
+                {
+                    Console.WriteLine("Press [ESC] to close SCLoader.");
+                } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+            }
+            catch (InvalidOperationException)
+            {
+
+                // No console available
+                // Could be a worker process in a cloud environment or something
+                // Just let the current thread sleep to run the application forever..
+                new ManualResetEvent(false).WaitOne();
+
+            }
 
         }
 
